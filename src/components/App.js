c2059640d660d2b57e8eb46c55071d41029665f9
surajvais <37078537+surajvais@users.noreply.github.com>
2020-11-27 15:49:51 +0530

    j

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,53 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor() {
+    super();
+    let date = new Date();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+      let ampm = hours >= 12 ? "PM" : "AM";
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      let strTime = hours + ":" + minutes + ":" + seconds +" "+ ampm;
+    this.state = { time: strTime };
+  }
+  componentDidMount() {
+    console.log("didMount");
+    this.intId = setInterval(() => {
+      let date = new Date();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+      let ampm = hours >= 12 ? "PM" : "AM";
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      let strTime = hours + ":" + minutes + ":" + seconds +" "+ ampm;
+      
+      this.setState({ time: strTime });
+    }, 1000);
+  }
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+  
+  componentWillUnmount() {
+    
+    clearInterval(this.intId);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="Clock">
+          <h3 id="time">{`${this.state.time}`}</h3>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default App;
